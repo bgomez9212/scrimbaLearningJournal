ccba0d6c0274f6db5e87a1beb979b1ade12e2728
Brandon Gomez <bgomez9212@gmail.com>
2024-02-27 16:31:02 -0800

    add deployment files + some fixes

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-document.addEventListener('click', function(e){
-    if(e.target.id === 'view-more'){
-        document.getElementById('more-articles').style.display = 'block'
-        document.getElementById('more').style.display = 'none'
-        document.getElementById('less').style.display = 'flex'
-    } else if (e.target.id === 'view-less'){
-        document.getElementById('more-articles').style.display = 'none'
-        document.getElementById('more').style.display = 'flex'
-        document.getElementById('less').style.display = 'none'
+let hiddenEls = document.getElementsByClassName("hidden");
+
+document.addEventListener("click", function (e) {
+  if (e.target.id === "view-more") {
+    for (let element of hiddenEls) {
+      element.style.display = "block";
     }
-})
+    document.getElementById("more").style.display = "none";
+    document.getElementById("less").style.display = "flex";
+  } else if (e.target.id === "view-less") {
+    document.getElementById("less").style.display = "none";
+    document.getElementById("more").style.display = "flex";
+    for (let element of hiddenEls) {
+      element.style.display = "none";
+    }
+  }
+});
